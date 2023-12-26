254cf9131e3b7be856a803de08e51f786b36bc67
Colin Brown <colin2003az@gmail.com>
2023-12-25 23:25:31 -0700

    final ui change?

---
+++ lednew/assets/QCardSection.be015210.js
@@ -0,0 +1,1 @@
+import{c as e,b as r,f as n,j as s}from"./index.4965cb06.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(a,{slots:t}){const o=r(()=>`q-card__section q-card__section--${a.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(a.tag,{class:o.value},s(t.default))}});export{i as Q};
